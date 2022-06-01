371bf9639af2d32d6900e8bc578085f3c6eaa398
Patrick Watson <ptwatson15@gmail.com>
2022-06-01 20:34:55 +0800

    merge

--- helloworld.py
+++ helloworld.py
@@ -1,18 +1,9 @@
-<<<<<<< HEAD
-def hello_world(state):
-    print(f'Hello World {state}!')
+def hello_world(city, state):
+    print(f'Hello World {city}, {state}!')
 
 
 def main():
-    hello_world('New York')
-=======
-def hello_world(city):
-    print(f'Hello World {city}!')
-
-
-def main():
-    hello_world('NYC')
->>>>>>> db3f38c8bece65b373907b43b51598579739bce9
+    hello_world('NYC', 'New York')
 
 
 main()
